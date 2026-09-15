fb582315ada3382782e6ea847774f866d0507082
ingrid <ingrid.d@edu.senai.br>
2026-09-15 10:47:42 -0300

    salvando

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const express = require('cors');
+const cors = require('cors');
 
 // import express from "express";
 // impor cors from "cors";
@@ -8,3 +8,33 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// {
+// "nome" : "Pedro",
+// "idade" : 20,
+//}
+
+let users = ALUNOS = [
+    {id: 1, nome: "Alice", curso: "Desenvolvimento de Sistemas"},
+    {id: 2, nome: "Brenda", curso: "Redes de Computadores"},
+    {id: 3, nome: "Brenno", curso: "Administração"},
+    {id: 4, nome: "Carlos", curso: "Desenvolvimento"},
+];
+
+app.get("/", (req, res) => {
+    res.json({
+        Mensagem: "API alunos funcionando"
+    });
+});
+
+app.get("/alunos",(req,res)=> {
+    res.json(ALUNOS);
+})
+
+const PORTA = 3000;
+
+app.listen(PORTA, () => {
+    console.log(`Servidor iniciado com sucesso!`);
+    console.log(`http://localhost:${PORTA}`);
+});
+
